670a6f4d5e729450719291fc6acdcc91734de24b
Vaarteen <herbert.caffarel@cicef.pro>
2019-05-01 16:20:07 +0200

    Exo11 fini

---
+++ src/java/actions/LoginAction.java
@@ -0,0 +1,37 @@
+package actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Herbert Caffarel <herbert.caffarel@cicef.pro>
+ */
+public class LoginAction extends ActionSupport {
+    private String identifier = "";
+    private String password = "";
+
+    @Override
+    public String execute() throws Exception {
+        if (!(identifier.equals("admin") && password.equals("houba"))) {
+            return INPUT;
+        };
+        return SUCCESS;
+    }
+    // Setters & Getters
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
